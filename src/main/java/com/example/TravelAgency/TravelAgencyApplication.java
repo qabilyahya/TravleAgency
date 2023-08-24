1704263b93c3dd0e2539cb4d131b92d06351de56
yahya <qabilyahya3@gmail.com>
2023-08-24 21:13:22 +0100

    heey

---
+++ src/main/java/com/example/TravelAgency/TravelAgencyApplication.java
@@ -0,0 +1,12 @@
+package com.example.TravelAgency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class TravelAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAgencyApplication.class, args);
+	}
+
+}
